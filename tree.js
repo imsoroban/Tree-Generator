ba7a319ebe10a3c509137fc16d583173768c5aea
Soroban <247510551+imsoroban@users.noreply.github.com>
2026-09-03 14:49:16 +1200

    fixed

--- tree.js
+++ tree.js
@@ -2,9 +2,11 @@ export default class Tree {
   constructor(ctx, config) {
     this.ctx = ctx;
     this.config = config;
-    this.branchcount = 0;
+    this.branchCount = 0;
   }
-  
+
   setConfig(patch) {
     this.config = { ...this.config, ...patch };
   }
+
+  render(width, height)
