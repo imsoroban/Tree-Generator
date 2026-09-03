70c78b11f73800066d2a6556b7ec89e6ccbefac9
Soroban <247510551+imsoroban@users.noreply.github.com>
2026-09-03 15:49:35 +1200

    JS I HATE JS

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ import Tree from "./Tree.js";
 const canvas = document.getElementById("treeCanvas");
 const ctx = canvas.getContext("2d");
 const canvasContainer = document.getElementById("canvasContainer");
+
+const inputs = {
+    depth: document.getElementById("depth"),
+  angle: document.getElementById("angle"),
+  decay: document.getElementById("decay"),
+  trunkHeight: document.getElementById("trunkHeight"),
+  trunkWidth: document.getElementById("trunkWidth"),
+  startHue: document.getElementById("startHue"),
+  tipHue: document.getElementById("tipHue"),
+};
