828b30e184b8cf36b6d62336b0094507fe2d3283
Soroban <247510551+imsoroban@users.noreply.github.com>
2026-09-03 15:47:37 +1200

    im mclovin this damn JS HUHDFHEIGFeowihfawoighreoiuh

---
+++ main.js
@@ -0,0 +1,5 @@
+import Tree from "./Tree.js";
+
+const canvas = document.getElementById("treeCanvas");
+const ctx = canvas.getContext("2d");
+const canvasContainer = document.getElementById("canvasContainer");
